105209e858ca25c642a2d872b33bc3e6a9405970
d.grigoriev <dmitry-gr87@yandex.ru>
2020-04-02 11:38:35 +0300

    Test commit 33

--- src/core/functions.rs
+++ src/core/functions.rs
@@ -25,7 +25,7 @@ impl ComposerIntro {
     }
 
 
-    pub fn get_full(tree_params: TreeParams, brief_params: BriefParams, update: Option<bool>, tree_order_key: &str, filter: Option<serde_json::Value>, id_key: Option<&str>) -> ResultParse<Vec<serde_json::Value>> {
+    pub fn get_full(tree_params: TreeParams, brief_params: BriefParams, update: Option<bool>, tree_order_key: &str, filter: Option<&serde_json::Value>, id_key: Option<&str>) -> ResultParse<Vec<serde_json::Value>> {
         let coll = mongo_get_coll(&brief_params.tmp_db_uri, &brief_params.tmp_db_name, &brief_params.app_type);
         let filter = convert_to_doc(filter);
         let is_force_update = update.unwrap_or(false);
