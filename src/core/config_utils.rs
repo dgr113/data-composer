0e5248053a87620db2adb89f0076f5c8d3090553
d.grigoriev <dmitry-gr87@yandex.ru>
2020-04-03 01:17:25 +0300

    Test commit 72

--- src/core/config_utils.rs
+++ src/core/config_utils.rs
@@ -26,8 +26,6 @@ impl<'a> TreeParams<'a> {
 pub struct BriefParams<'a> {
     pub app_type: String,
     pub tree_path: String,
-    pub tmp_db_uri: String,
-    pub tmp_db_name: String,
     pub order_path: String,
     pub brief_fields: Vec<String>,
     pub access_key: &'a [&'a str]
@@ -37,8 +35,6 @@ impl<'a> BriefParams<'a> {
     pub fn build_params(config: &'a HashMap<String, String>, app_type: &'a str, access_key: &'a [&'a str]) -> BriefParams<'a> {
         let tree_path = build_path(&config["TREES_BASEDIR"], &build_filename(&app_type,None, "yml", "."));
         let order_path = build_path(&config["ORDERS_BASEDIR"], &build_filename(&app_type, None, "order", "."));
-        let tmp_db_uri = &config["TMP_DB_URI"];
-        let tmp_db_name = &config["TMP_DB_NAME"];
 
         let brief_fields = &config["BRIEFLY_FIELDS"].as_str().split(",").map(|s| s.trim().to_string()).collect::<Vec<String>>();
         BriefParams {
@@ -46,8 +42,6 @@ impl<'a> BriefParams<'a> {
             order_path,
             access_key,
             app_type: app_type.to_string(),
-            tmp_db_uri: tmp_db_uri.clone(),
-            tmp_db_name: tmp_db_name.clone(),
             brief_fields: brief_fields.to_vec(),
         }
     }
