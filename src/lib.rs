c67c7a700d03ca1b90ffe6f93cfaa2827dbbf924
d.grigoriev <dmitry-gr87@yandex.ru>
2021-08-28 22:14:07 +0300

    Update dependencies version 10

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ pub extern crate data_finder;
 pub extern crate data_getter;
 
 use std::io;
+use std::hash::Hash;
+use std::path::Path;
+use std::ffi::OsStr;
+use std::borrow::Borrow;
 
 use mongodb::sync::Collection;
 use serde_json::Value as SerdeJsonValue;
@@ -14,8 +18,6 @@ pub use crate::core::functions::ComposerIntro;
 use data_getter::ResultParse;
 use data_getter::GetterConfig;
 use data_finder::config::FinderConfig;
-use std::hash::Hash;
-use std::borrow::Borrow;
 
 
 
@@ -28,32 +30,34 @@ impl ComposerApi {
     * # Parameters:
     * `id_key`: Field of every document in <arr_data> interpreted as database document ID
     */
-    pub fn get_full<S>(
+    pub fn get_full<S, K, P>(
         finder_config: &FinderConfig,
         getter_config: &GetterConfig,
         app_type: S,
         coll: &Collection,
-        access_key: &[S],
+        access_key: &[K],
         update: Option<bool>,
         filter: Option<&SerdeJsonValue>,
         id_key: Option<&str>,
-        tree_path: &str
+        tree_path: P
     )
         -> ResultParse<Vec<SerdeJsonValue>>
-            where S: Into<String> + ToString + Hash + Eq + serde_yaml::Index, String: Borrow<S>
+        where S: Into<String> + Hash + Eq, String: Borrow<S>,
+              K: Into<String> + Hash + Eq + serde_yaml::Index, String: Borrow<K>,
+              P: AsRef<Path> + AsRef<OsStr>
     {
         // let access_key = &[lang, ];
         // let params = Params::build_params(getter_config, app_type, access_key);
         // ComposerIntro::get_full(params, finder_config, coll, update, filter, id_key)
-
         ComposerIntro::get_full(getter_config, finder_config, coll, update, filter, id_key, app_type, tree_path, access_key)
     }
 
 
     /** Get a brief description of a given content type */
-    pub fn get_tree<S>(finder_config: &FinderConfig, app_type: S, tree_path: &str)
+    pub fn get_tree<S, P>(finder_config: &FinderConfig, app_type: S, tree_path: P)
         -> Result<serde_yaml::Value, io::Error>
-            where S: Into<String> + Hash + Eq, String: Borrow<S>
+            where S: Into<String> + Hash + Eq, String: Borrow<S>,
+                  P: AsRef<Path> + AsRef<OsStr>
     {
         // let access_key = &["", ];
         // let params = Params::build_params(getter_config, app_type, access_key);
