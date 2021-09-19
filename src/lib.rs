432a25536887852a0d9840a92f9c527fe2447ab5
d.grigoriev <dmitry-gr87@yandex.ru>
2021-09-19 13:01:45 +0300

    Update dependencies version 20

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,12 @@ pub mod core;
 pub mod errors;
 pub mod config;
 
-use data_getter::ResultParse;
 use data_finder::config::FinderConfig;
 
+pub use crate::errors::ApiError;
 pub use crate::config::ComposerConfig;
 pub use crate::core::config_utils::Params;
 pub use crate::core::functions::ComposerIntro;
-pub use crate::errors::ApiError;
 
 
 
@@ -43,12 +42,13 @@ impl ComposerApi {
         id_key: Option<&str>,
         tree_path: P
     )
-        -> ResultParse<Vec<SerdeJsonValue>>
+        -> Result<Vec<SerdeJsonValue>, ApiError>
             where S: Into<String> + Hash + Eq, String: Borrow<S>,
                   K: Into<String> + Hash + Eq + serde_yaml::Index, String: Borrow<K>,
                   P: AsRef<Path> + AsRef<OsStr>
     {
-        ComposerIntro::get_full(composer_config, coll, update, filter, id_key, app_type, tree_path, access_key)
+        let result = ComposerIntro::get_full(composer_config, coll, update, filter, id_key, app_type, tree_path, access_key) ?;
+        Ok( result )
     }
 
 
